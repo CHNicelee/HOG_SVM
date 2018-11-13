f03dd37cd49957048a9350a0d310e99463c0ae01
Icelee <1242027878@qq.com>
2018-11-13 14:41:51 +0800

    Update hog_svm.py
    transfer to windows

--- hog_svm.py
+++ hog_svm.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 import glob
+import platform
 import time
 from PIL import Image
 from skimage.feature import hog
@@ -8,23 +9,21 @@ import os
 import joblib
 from sklearn.svm import LinearSVC
 import shutil
-from matplotlib import pyplot
 
-#第一个是你的类别   第二个是类别对应的名称   输出结果的时候方便查看
-label_map = {0:'train',
-             1:'bus',
-             2:'person',
-             3:'cat',
-             4:'car'}
-#训练集图片的位置
-train_image_path = '/home/icelee/Desktop/python/VOCdevkit/VOC2012/small_image/'
-#测试集图片的位置
-test_image_path = '/home/icelee/Downloads/dataset/small_shixun/'
+# 第一个是你的类别   第二个是类别对应的名称   输出结果的时候方便查看
+label_map = {1: 'cat',
+             2: 'chick',
+             3: 'snack',
+             }
+# 训练集图片的位置
+train_image_path = 'E:\\pythonDir\\HOG_SVM\\image'
+# 测试集图片的位置
+test_image_path = 'E:\\pythonDir\\HOG_SVM\\image'
 
-#训练集标签的位置
-train_label_path = '/home/icelee/Desktop/python/VOCdevkit/VOC2012/mydata.txt'
-#测试集标签的位置
-test_label_path = '/home/icelee/Downloads/dataset/train.txt'
+# 训练集标签的位置
+train_label_path = 'E:\\pythonDir\\HOG_SVM\\image\\train.txt'
+# 测试集标签的位置
+test_label_path = 'E:\\pythonDir\\HOG_SVM\\image\\train.txt'
 
 size = 128
 
@@ -33,60 +32,66 @@ test_feat_path = 'test/'
 model_path = 'model/'
 
 
-#获得图片列表
-def get_image_list(filePath,nameList):
+# 获得图片列表
+def get_image_list(filePath, nameList):
+    print('read image from ',filePath)
     img_list = []
     for name in nameList:
-        temp = Image.open(filePath+name)
+        temp = Image.open(os.path.join(filePath,name))
         img_list.append(temp.copy())
         temp.close()
     return img_list
 
-#提取特征并保存
-def get_feat(image_list,name_list,label_list,savePath,size):
+
+# 提取特征并保存
+def get_feat(image_list, name_list, label_list, savePath, size):
     i = 0
     for image in image_list:
         try:
-            #如果是灰度图片  把3改为-1
+            # 如果是灰度图片  把3改为-1
             image = np.reshape(image, (size, size, 3))
         except:
-            print name_list[i]
+            print('发送了异常，图片大小size不满足要求：',name_list[i])
             continue
-        gray = rgb2gray(image)/255.0
-        #这句话根据你的尺寸改改
-        fd = hog(gray, orientations=12, pixels_per_cell=[8,8], cells_per_block=[4,4], visualise=False, transform_sqrt=True)
+        gray = rgb2gray(image) / 255.0
+        # 这句话根据你的尺寸改改
+        fd = hog(gray, orientations=12,block_norm='L1', pixels_per_cell=[8, 8], cells_per_block=[4, 4], visualize=False,
+                 transform_sqrt=True)
         fd = np.concatenate((fd, [label_list[i]]))
-        fd_name = name_list[i]+'.feat'
+        fd_name = name_list[i] + '.feat'
         fd_path = os.path.join(savePath, fd_name)
         joblib.dump(fd, fd_path)
         i += 1
-    print "Test features are extracted and saved."
+    print("Test features are extracted and saved.")
 
-#变成灰度图片
+
+# 变成灰度图片
 def rgb2gray(im):
-    gray = im[:, :, 0]*0.2989+im[:, :, 1]*0.5870+im[:, :, 2]*0.1140
+    gray = im[:, :, 0] * 0.2989 + im[:, :, 1] * 0.5870 + im[:, :, 2] * 0.1140
     return gray
 
-#获得图片名称与对应的类别
+
+# 获得图片名称与对应的类别
 def get_name_label(file_path):
+    print("read label from ",file_path)
     name_list = []
     label_list = []
     with open(file_path) as f:
         for line in f.readlines():
             name_list.append(line.split(' ')[0])
             label_list.append(line.split(' ')[1])
-    return name_list,label_list
+    return name_list, label_list
 
 
-#提取特征
+# 提取特征
 def extra_feat():
-    train_name,train_label = get_name_label(train_label_path)
-    test_name,test_label = get_name_label(test_label_path)
+    train_name, train_label = get_name_label(train_label_path)
+    test_name, test_label = get_name_label(test_label_path)
 
-    train_image = get_image_list(train_image_path,train_name)
-    test_image = get_image_list(test_image_path,test_name)
-    get_feat(train_image,train_name,train_label,train_feat_path,size)
-    get_feat(test_image,test_name,test_label,test_feat_path,size)
+    train_image = get_image_list(train_image_path, train_name)
+    test_image = get_image_list(test_image_path, test_name)
+    get_feat(train_image, train_name, train_label, train_feat_path, size)
+    get_feat(test_image, test_name, test_label, test_feat_path, size)
 
 
 # 创建存放特征的文件夹
@@ -96,7 +101,8 @@ def mkdir():
     if not os.path.exists(test_feat_path):
         os.mkdir(test_feat_path)
 
-#训练和测试
+
+# 训练和测试
 def train_and_test():
     t0 = time.time()
     clf_type = 'LIN_SVM'
@@ -110,61 +116,63 @@ def train_and_test():
             data = joblib.load(feat_path)
             fds.append(data[:-1])
             labels.append(data[-1])
-        print "Training a Linear LinearSVM Classifier."
+        print("Training a Linear LinearSVM Classifier.")
         clf = LinearSVC()
         clf.fit(fds, labels)
         # 下面的代码是保存模型的
         if not os.path.exists(model_path):
             os.makedirs(model_path)
-        joblib.dump(clf, model_path+'model')
-        #下面的代码是加载模型  可以注释上面的代码   直接进行加载模型  不进行训练
+        joblib.dump(clf, model_path + 'model')
+        # 下面的代码是加载模型  可以注释上面的代码   直接进行加载模型  不进行训练
         # clf = joblib.load(model_path+'model')
-        print "训练之后的模型存放在model文件夹中"
-        #exit()
-        train_name, train_label = get_name_label(train_label_path)
-        test_name, test_label = get_name_label(test_label_path)
+        print("训练之后的模型存放在model文件夹中")
+        # exit()
         result_list = []
         for feat_path in glob.glob(os.path.join(test_feat_path, '*.feat')):
             total += 1
-            image_name = feat_path.split('/')[1].split('.feat')[0]
+            if platform.system() == 'Windows':
+                symbol = '\\'
+            else:
+                symbol = '/'
+            image_name = feat_path.split(symbol)[1].split('.feat')[0]
             data_test = joblib.load(feat_path)
-            data_test_feat = data_test[:-1].reshape((1,-1)).astype(np.float64)
+            data_test_feat = data_test[:-1].reshape((1, -1)).astype(np.float64)
             result = clf.predict(data_test_feat)
-            result_list.append(image_name+' '+label_map[int(result[0])]+'\n')
-            
+            result_list.append(image_name + ' ' + label_map[int(result[0])] + '\n')
+
             if int(result[0]) == int(data_test[-1]):
                 num += 1
         write_to_txt(result_list)
-        rate = float(num)/total
+        rate = float(num) / total
         t1 = time.time()
-        print '准确率是： %f'%rate
-        print '耗时是 : %f'%(t1-t0)
+        print('准确率是： %f' % rate)
+        print('耗时是 : %f' % (t1 - t0))
+
 
 def write_to_txt(list):
-    with open('result.txt','w') as f:
+    with open('result.txt', 'w') as f:
         f.writelines(list)
-    print '每张图片的识别结果存放在result.txt里面'
+    print('每张图片的识别结果存放在result.txt里面')
+
 
 if __name__ == '__main__':
 
     mkdir()  # 不存在文件夹就创建
+    # need_input = input('是否手动输入各个信息？y/n\n')
 
-    need_input = raw_input('是否手动输入各个信息？y/n\n')
+    # if need_input == 'y':
+    #     train_image_path = input('请输入训练图片文件夹的位置,如 /home/icelee/image\n')
+    #     test_image_path = input('请输入测试图片文件夹的位置,如 /home/icelee/image\n')
+    #     train_label_path = input('请输入训练集合标签的位置,如 /home/icelee/train.txt\n')
+    #     test_label_path = input('请输入测试集合标签的位置,如 /home/icelee/test.txt\n')
+    #     size = int(input('请输入您图片的大小：如64x64，则输入64\n'))
 
-    if need_input == 'y':
-        train_image_path = raw_input('请输入训练图片文件夹的位置,如 /home/icelee/image/ 结尾一定要带有分隔符\n')
-        test_image_path = raw_input('请输入测试图片文件夹的位置,如 /home/icelee/image/ 结尾一定要带有分隔符\n')
-        train_label_path = raw_input('请输入训练集合标签的位置,如 /home/icelee/train.txt\n')
-        test_label_path = raw_input('请输入测试集合标签的位置,如 /home/icelee/test.txt\n')
-        size = int(raw_input('请输入您图片的大小：如64x64，则输入64\n'))
-
-    need_extra_feat = raw_input('是否需要重新获取特征？y/n\n')
+    need_extra_feat = input('是否需要重新获取特征？y/n\n')
 
     if need_extra_feat == 'y':
         shutil.rmtree(train_feat_path)
         shutil.rmtree(test_feat_path)
         mkdir()
-        extra_feat()#获取特征并保存在文件夹
+        extra_feat()  # 获取特征并保存在文件夹
 
-    train_and_test() #训练并预测
-
+    train_and_test()  # 训练并预测
