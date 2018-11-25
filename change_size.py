7a71971822ce4e8fc39ab1abc78626f9eb8bfc16
CHNicelee <1242027878@qq.com>
2018-11-25 10:29:01 +0800

    添加resize代码

---
+++ change_size.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+from PIL import Image
+import os
+image_height = 128
+image_width = 100
+def fixed_size(filePath,savePath):
+    """按照固定尺寸处理图片"""
+    im = Image.open(filePath)
+    out = im.resize((image_height, image_width), Image.ANTIALIAS)
+    out.save(savePath)
+
+
+def changeSize():
+    filePath = r'E:\pythonDir\tmp\image'
+    destPath = r'E:\pythonDir\tmp\image128'
+    if not os.path.exists(destPath):
+        os.makedirs(destPath)
+    for root, dirs, files in os.walk(filePath):
+        for file in files:
+            if file[-1]=='g':
+                fixed_size(os.path.join(filePath, file), os.path.join(destPath, file))
+    print('Done')
+
+if __name__ == '__main__':
+    changeSize()
